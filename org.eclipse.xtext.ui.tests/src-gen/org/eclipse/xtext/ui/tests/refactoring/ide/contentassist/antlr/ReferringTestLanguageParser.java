529dd6148bbb82f6ca5dcee2bc8726487e804eba
Moritz Eysholdt <moritz.eysholdt@typefox.io>
2016-08-11 07:54:03 +0200

    regenerated languages
    Signed-off-by: Moritz Eysholdt <moritz.eysholdt@typefox.io>

--- org.eclipse.xtext.ui.tests/src-gen/org/eclipse/xtext/ui/tests/refactoring/ide/contentassist/antlr/ReferringTestLanguageParser.java
+++ org.eclipse.xtext.ui.tests/src-gen/org/eclipse/xtext/ui/tests/refactoring/ide/contentassist/antlr/ReferringTestLanguageParser.java
@@ -4,14 +4,10 @@
 package org.eclipse.xtext.ui.tests.refactoring.ide.contentassist.antlr;
 
 import com.google.inject.Inject;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import org.antlr.runtime.RecognitionException;
 import org.eclipse.xtext.AbstractElement;
 import org.eclipse.xtext.ide.editor.contentassist.antlr.AbstractContentAssistParser;
-import org.eclipse.xtext.ide.editor.contentassist.antlr.FollowElement;
-import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.AbstractInternalContentAssistParser;
 import org.eclipse.xtext.ui.tests.refactoring.ide.contentassist.antlr.internal.InternalReferringTestLanguageParser;
 import org.eclipse.xtext.ui.tests.refactoring.services.ReferringTestLanguageGrammarAccess;
 
@@ -45,18 +41,7 @@ public class ReferringTestLanguageParser extends AbstractContentAssistParser {
 		}
 		return nameMappings.get(element);
 	}
-
-	@Override
-	protected Collection<FollowElement> getFollowElements(AbstractInternalContentAssistParser parser) {
-		try {
-			InternalReferringTestLanguageParser typedParser = (InternalReferringTestLanguageParser) parser;
-			typedParser.entryRuleMain();
-			return typedParser.getFollowElements();
-		} catch(RecognitionException ex) {
-			throw new RuntimeException(ex);
-		}
-	}
-
+			
 	@Override
 	protected String[] getInitialHiddenTokens() {
 		return new String[] { "RULE_WS", "RULE_ML_COMMENT", "RULE_SL_COMMENT" };
