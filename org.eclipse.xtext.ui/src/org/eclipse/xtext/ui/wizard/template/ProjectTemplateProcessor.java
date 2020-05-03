bd8eaaff7741ccb0ec87d719787b903fbcfe9b75
nbhusare <neerajbhusare@gmail.com>
2020-05-03 17:45:34 +0530

    Migrated Xtend code from the org.eclipse.xtext.ui project to Java
    Signed-off-by: nbhusare <neerajbhusare@gmail.com>

--- org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/wizard/template/ProjectTemplateProcessor.java
+++ org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/wizard/template/ProjectTemplateProcessor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017, 2018 itemis AG (http://www.itemis.de) and others.
+ * Copyright (c) 2017, 2020 itemis AG (http://www.itemis.de) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -16,7 +16,6 @@ import org.eclipse.xtend.lib.macro.declaration.MutableClassDeclaration;
 import org.eclipse.xtext.ui.wizard.template.AbstractProjectTemplate;
 import org.eclipse.xtext.ui.wizard.template.ProjectTemplate;
 import org.eclipse.xtext.ui.wizard.template.TemplateProcessor;
-import org.eclipse.xtext.xbase.lib.Extension;
 
 /**
  * Generate some code to simplify implementation of project templates.
@@ -27,28 +26,27 @@ import org.eclipse.xtext.xbase.lib.Extension;
  * <li>Generate "Messages.java" for i18n</li>
  * </ol>
  * 
- * The generated files for i18n contain the "label" and "description" of all the project templates. The files may be
- * extended manually by the user to externalize more strings. The generator then merges its own changes into the
- * existing files.
+ * The generated files for i18n contain the "label" and "description" of all the project templates. The files may be extended manually by
+ * the user to externalize more strings. The generator then merges its own changes into the existing files.
  * 
  * @author Arne Deutsch - Initial contribution and API
  * @since 2.14
  */
 @Beta
-@SuppressWarnings("all")
 public class ProjectTemplateProcessor extends TemplateProcessor {
-  @Override
-  public void doTransform(final MutableClassDeclaration annotatedClass, @Extension final TransformationContext context) {
-    annotatedClass.setExtendedClass(context.newTypeReference(AbstractProjectTemplate.class));
-  }
-  
-  @Override
-  protected String getLabel(final ClassDeclaration annotatedClass, @Extension final CodeGenerationContext context) {
-    return annotatedClass.findAnnotation(context.findTypeGlobally(ProjectTemplate.class)).getStringValue("label");
-  }
-  
-  @Override
-  protected String getDescription(final ClassDeclaration annotatedClass, @Extension final CodeGenerationContext context) {
-    return annotatedClass.findAnnotation(context.findTypeGlobally(ProjectTemplate.class)).getStringValue("description");
-  }
+
+	@Override
+	public void doTransform(MutableClassDeclaration annotatedClass, TransformationContext context) {
+		annotatedClass.setExtendedClass(context.newTypeReference(AbstractProjectTemplate.class));
+	}
+
+	@Override
+	protected String getLabel(ClassDeclaration annotatedClass, CodeGenerationContext context) {
+		return annotatedClass.findAnnotation(context.findTypeGlobally(ProjectTemplate.class)).getStringValue("label");
+	}
+
+	@Override
+	protected String getDescription(ClassDeclaration annotatedClass, CodeGenerationContext context) {
+		return annotatedClass.findAnnotation(context.findTypeGlobally(ProjectTemplate.class)).getStringValue("description");
+	}
 }
