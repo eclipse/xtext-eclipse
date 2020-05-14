8664bdcc181dc0006c93c5f538ed2bd1b4d73786
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2020-05-14 11:34:50 +0200

    [#1466] Review feedback: Update copyrights

--- org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/DirtyStateManager.java
+++ org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/DirtyStateManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2009, 2020 itemis AG (http://www.itemis.eu) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -32,7 +32,6 @@ import com.google.common.collect.MapMaker;
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-// TODO: batch events according to the contract of IDirtyStateManager
 public class DirtyStateManager extends AbstractResourceDescriptionChangeEventSource implements IDirtyStateManager, IDirtyStateManagerExtension {
 
 	private ConcurrentMap<URI, IDirtyResource> managedResources;
